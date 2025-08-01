5122f8ad8a56f812e70fa14a4557bbda0c652816
Neo-Theophylline <theophyllineneo@gmail.com>
2025-08-01 15:41:10 +0700

    post data

--- resources/views/siswa/create.blade.php
+++ resources/views/siswa/create.blade.php
@@ -8,70 +8,68 @@
 </head>
 
 <body>
-    <a href=""></a>
     <h1>Halaman Create</h1>
-    <form action="" method="post">
-    @csrf
-<div>
-        <label for="">Kelas</label>
-        
-            <select name="kelas" id="kelas">
+    <form action="/siswa/store" method="post">
+        @csrf
+        <div>
+            <label for="">Kelas</label>
+            <select name="kelas">
                 <br>
-            <option value="">XII 1</option>
-            <option value="">XII 2</option>
-            <option value="">XII 3</option>
-        </select>
-    </div>
-<br>
-    <div>
-        <label for="">Nama</label>
+                <option value=1>XII 1</option>
+                <option value=2>XII 2</option>
+                <option value=3>XII 3</option>
+            </select>
+        </div>
         <br>
-        <input type="text" name="nama" placeholder="Masukkan Nama">
-    </div>
-<br>
-    <div>
-        <label for="">Nisn</label>
+        <div>
+            <label for="">Nama</label>
+            <br>
+            <input type="text" name="name" placeholder="Masukkan Nama">
+        </div>
         <br>
-        <input type="text" name="nisn" placeholder="Masukkan NISN">
-    </div>
-<br>
-    <div>
-        <label for="">Alamat</label>
+        <div>
+            <label for="">Nisn</label>
+            <br>
+            <input type="text" name="nisn" placeholder="Masukkan NISN">
+        </div>
         <br>
-        <input type="text" name="alamat" placeholder="Masukkan Alamat">
-    </div>
-<br>
-    <div>
-        <label for="">Email</label>
+        <div>
+            <label for="">Alamat</label>
+            <br>
+            <input type="text" name="alamat" placeholder="Masukkan Alamat">
+        </div>
         <br>
-        <input type="text" name="email" placeholder="Masukkan Email">
-    </div>
-<br>
-    <div>
-        <label for="">Password</label>
+        <div>
+            <label for="">Email</label>
+            <br>
+            <input type="text" name="email" placeholder="Masukkan Email">
+        </div>
         <br>
-        <input type="password" name="password" placeholder="Masukkan Password">
-    </div>
-<br>
-    <div>
-        <label for="">No Handphone</label>
+        <div>
+            <label for="">Password</label>
+            <br>
+            <input type="password" name="password" placeholder="Masukkan Password">
+        </div>
         <br>
-        <input type="text" name="no_handphone" placeholder="Masukkan No Handphone">
-    </div>
-<br>
-    <div>
-        <label for="">Foto</label>
+        <div>
+            <label for="">No Handphone</label>
+            <br>
+            <input type="text" name="no_handphone" placeholder="Masukkan No Handphone">
+        </div>
         <br>
-        <input type="file" name="photo" accept="image/*">
-    </div>
-    <br>
-    <button type="submit" href="">
-        Simpan
-    </button>
-    <br>
-    <br>
+        <div>
+            <label for="">Foto</label>
+            <br>
+            <input type="file" name="photo" accept="image/*">
+        </div>
+        <br>
+        <button type="submit" href="/store">
+            Simpan
+        </button>
+        <br>
+        <br>
 
-</form>
+    </form>
     <a href="/"><button>Ngga Jdi</button></a>
 </body>
 
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,43 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Tugas</h1>
     <div>
         <table border="1">
             <div>
-        <thead>
-            <tr>
-                <td>Foto</td>
-                <td>Name</td>
-                <td>Kelas</td>
-                <td>Alamat</td>
-                <td>Opsi</td>
-            </tr>
-        </thead>
-    </div>
-    <div>
-        <tbody>
-            <tr>
-                <td>png</td>
-                <td>xixixi</td>
-                <td>12</td>
-                <td>kulur</td>
-                <td>
-                    <a href="detail">detail</a>
-                    |
-                    <a href="delete">delete</a>
-                    |
-                    <a href="create">create</a>
-                </td>
-            </tr>
-        </tbody>
-    </div>
-    </table>
+                <thead>
+                    <tr>
+                        <td>Foto</td>
+                        <td>Name</td>
+                        <td>Kelas</td>
+                        <td>Alamat</td>
+                        <td>Opsi</td>
+                    </tr>
+                </thead>
+            </div>
+            <div>
+                <tbody>
+                    <tr>
+                        <td>png</td>
+                        <td>xixixi</td>
+                        <td>12</td>
+                        <td>kulur</td>
+                        <td>
+                            <a href="#">detail</a>
+                            |
+                            <a href="#">delete</a>
+                            |
+                            <a href="#">edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </div>
+        </table>
+        <br>
+        <div>
+            <a href="create"><button>create</button></a>
+        </div>
+        <br>
     </div>
 </body>
+
 </html>

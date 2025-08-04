5dad6d1159801847a87d0ac18bba86dc6b6830d0
Neo-Theophylline <theophyllineneo@gmail.com>
2025-08-04 10:09:45 +0700

    adding error massege

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -4,22 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\user;
+use Illuminate\Validation\Rules\Unique;
 
 class SiswaController extends Controller
 {
+
+
+    public function index()
+    {
+        return view('siswa.index');
+    }
+
+    public function create()
+    {
+        return view('siswa.create');
+    }
+
+
+
     public function store(Request $request)
     {
         //validate
         $request->validate([
             'kelas' => 'required',
             'name' => 'required',
-            'nisn' => 'required',
+            'nisn' => 'required|unique:users,nisn',
             'alamat' => 'required',
-            'email' => 'required',
+            'email' => 'required|unique:users,email',
             'password' => 'required',
             'no_handphone' => 'required',
         ]);
-    
+
         $datauser = [
             'clas_id' => $request->kelas,
             'photo' => 'photo.jpg',
@@ -34,6 +49,5 @@ class SiswaController extends Controller
         user::create($datauser);
 
         return redirect('/');
-    }   
-
+    }
 }
